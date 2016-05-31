d550e6c31cbcd42778f1e15b0333a87eae86229c
Chukwuemeka Afigbo <eafigbo@fb.com>
2016-05-31 21:17:31 +0100

    added css and javascript using html5boilerplate.com. Also added ability to navigate with arrow key and and scored count

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from bottle import route, run, jinja2_template as template, redirect,abort,jinja2_view as view,url,request
+from bottle import route, run, jinja2_template as template, redirect,abort,jinja2_view as view,url,request,static_file
 import jinja2
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from pagnation import Pagination
 from math import ceil
+import urllib
+import os
 
 PER_PAGE = 1
 
@@ -18,6 +20,9 @@ db = client.test
 
 def count_all_docs():
   return db["documents"].count()
+
+def count_all_scored_docs():
+  return db["documents"].find({'score':{ '$exists': True, '$ne':''}}).count()
 
 def get_docs_for_page(page, PER_PAGE, count):
   print "count is "+ str(ceil(count/float(PER_PAGE)))
@@ -56,11 +61,15 @@ def show_docs(page=1):
   if not docs or page < 1:
     abort(404)
   pagination = Pagination(page, PER_PAGE, count)
+  message = request.query.message
   return {
     "pagination":pagination,
     "docs":docs,
     "url_for_other_page":url_for_other_page,
+    "count_all_scored_docs":count_all_scored_docs,
+    "count_all_docs":count_all_docs,
     "sorted":sorted,
+    "message":message
   }
 
 @route('/docs/list/<page:int>', method='POST')
@@ -68,15 +77,36 @@ def show_docs(page=1):
 def show_docs(page=1):
   the_id = request.forms.get('_id')
   the_score = request.forms.get('score')
+  message = ''
   if the_id and the_score and the_score.isdigit() :
     the_doc = db["documents"].find_one({"_id":ObjectId(the_id)})
     the_doc["score"] = the_score
     the_doc = db["documents"].save(the_doc)
-    print "saved!"
+    message = "Score saved!"
+  elif not the_id:
+    message = "Error : id not in request "
+  elif not the_score:
+    message = "Error : Score not in request "
+  elif not the_score.isdigit():
+    message = "Error : Score must be a number between 0 and 10 "
 
-  redirect("/docs/list/"+str(page))
+  message = urllib.urlencode({"message":message})
+
+  redirect("/docs/list/"+str(page)+"?"+message)
 
 
+@route('/static/img/<filename:re:[^\s]+(\.(?i)(jpg|png|gif|bmp))$>')
+def send_image(filename):
+  the_filename, file_extension = os.path.splitext('filename')
+  return static_file(filename, root='static/img', mimetype='image/'+file_extension.replace('.',''))
+
+@route('/static/js/<filename:path>')
+def send_js(filename):
+  return static_file(filename, root='static/js')
+
+@route('/static/<filename:path>')
+def send_html(filename):
+  return static_file(filename, root='static')
 
 
 
---
+++ static/js/main.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+
+    $("html").keydown(function(e){
+      switch(e.which) {
+       case 37: // left
+        new_page_number = 1;
+        the_url = $(location).attr('href').split('?')[0];//with query strings
+        page_number = the_url.substr(the_url.lastIndexOf('/') + 1);
+        if(page_number > 1){
+          new_page_number = new Number(page_number)-1;
+        }
+        new_url = the_url.replace(page_number,'');
+        new_url = new_url+new_page_number;
+        window.location.href = new_url;
+        break;
+
+        /*case 38: // up
+        alert("up")
+        break;*/
+
+        case 39: // right
+        new_page_number = 1;
+        the_url = $(location).attr('href').split('?')[0];//with query strings
+        page_number = the_url.substr(the_url.lastIndexOf('/') + 1);
+        //if(page_number > 1){ no way yet to get max pages in JS, use ajax?
+          new_page_number = new Number(page_number) + 1;
+        //}
+        new_url = the_url.replace(page_number,'');
+        new_url = new_url+new_page_number;
+        window.location.href = new_url;
+        break;
+
+        /*case 40: // down
+        alert("down")
+        break;*/
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+
+
+});
